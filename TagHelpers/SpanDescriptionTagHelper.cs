a676415a51a9f09dc4029b1064871f982fd45162
jreddington76 <jreddington76@users.noreply.github.com>
2018-12-04 16:05:04 +0000

    moved payment code to new controller

---
+++ TagHelpers/SpanDescriptionTagHelper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace aspnetcore_sagepay.TagHelpers
+{
+    [HtmlTargetElement("span", Attributes = DescriptionForAttributeName)]
+    public class SpanDescriptionTagHelper : TagHelper
+    {
+        private const string DescriptionForAttributeName = "asp-description-for";
+
+        public SpanDescriptionTagHelper(IHtmlGenerator generator)
+        {
+            Generator = generator;
+        }
+
+        public override int Order
+        {
+            get
+            {
+                return -1000;
+            }
+        }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        protected IHtmlGenerator Generator { get; }
+
+        /// <summary>
+        /// An expression to be evaluated against the current model.
+        /// </summary>
+        [HtmlAttributeName(DescriptionForAttributeName)]
+        public ModelExpression DescriptionFor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            var metadata = DescriptionFor.Metadata ?? throw new InvalidOperationException($"No provided metadata ({DescriptionForAttributeName})");
+
+            output.Attributes.SetAttribute("id", metadata.PropertyName + "-description");
+
+            if (!string.IsNullOrWhiteSpace(metadata.Description))
+            {
+                output.Content.SetContent(metadata.Description);
+                output.TagMode = TagMode.StartTagAndEndTag;
+            }
+        }
+    }
+}
